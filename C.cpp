c7bb0c7ee71ba8dfc83599b8a2f503630d3f9168
latentghost <akshat21515@iiitd.ac.in>
2023-04-14 20:32:23 +0530

    accepted

--- C.cpp
+++ C.cpp
@@ -10,65 +10,71 @@ using namespace std;
 #define ll long long
 
 
-int djikstra(vector<vector<pair<int,pair<int,int>>>> &adj){
-    vector<int> lens(adj.size(),INT_MAX);
+ll djikstra(vector<vector<pair<ll,pair<ll,ll>>>> &adj, ll n){
+    vector<ll> lens(n,LLONG_MAX);
+    vector<int> vis(n,0);
     lens[0] = 0;
 
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
     pq.push({0,1});
     
     while(!pq.empty()){
-        int a = pq.top().second;
-        if(a==adj.size()){
-            return pq.top().first;
-        }
+        ll a = pq.top().second;
         pq.pop();
 
-        for(int i=0; i<adj[a-1].size(); i++){
-            int w = 0;
-            int f = (int) sqrt(adj[a-1][i].second.second);
-            if(lens[a-1]>=f)
-                w = adj[a-1][i].second.first + (adj[a-1][i].second.second/(1 + lens[a-1]));
-            
-            else
-                w = f - lens[a-1] + adj[a-1][i].second.first + adj[a-1][i].second.second/(1 + f);
-            
-            if(lens[a-1] + w < lens[adj[a-1][i].first-1]){
-                lens[adj[a-1][i].first-1] = lens[a-1] + w;
-                pq.push({lens[adj[a-1][i].first-1],adj[a-1][i].first});
+        if(vis[a-1]==0){   
+            for(auto &v: adj[a-1]){
+                ll w = 0;
+                
+                if(lens[a-1]>=((ll) floor(sqrt(v.second.second))))
+                    w = v.second.first + (v.second.second/(1 + lens[a-1]));
+                
+                else
+                    w = ((ll) floor(sqrt(v.second.second))) - lens[a-1] + v.second.first + v.second.second/(1 + ((ll) floor(sqrt(v.second.second))));
+                
+                if(lens[a-1] + w < lens[v.first-1]){
+                    lens[v.first-1] = lens[a-1] + w;
+                    pq.push({lens[v.first-1],v.first});
+                }
             }
         }
+        vis[a-1] = 1;
     }
 
-    return lens[adj.size()-1];
+    return lens[n-1];
 }
 
 
 int main()
 {
-    int n,m;
+    ll n,m;
     cin >> n >> m;
 
-    vector<vector<int>> inds(n,vector<int>(n,-1));
-    vector<vector<pair<int,pair<int,int>>>> adjl(n);
+    // vector<vector<pair<int,int>>> wts(n,vector<pair<int,int>>(n,make_pair(1e9+5,1e9+5)));
+
+    vector<vector<pair<ll,pair<ll,ll>>>> adjl(n);
 
     for(int i=0; i<m; i++){
-        int a,b,c,d;
+        ll a,b;
+        ll c,d;
         cin >> a >> b >> c >> d;
         
-        if(inds[a-1][b-1]==-1){
-            adjl[a-1].pb(b,make_pair(c,d));
-            adjl[b-1].pb(a,make_pair(c,d));
-            inds[a-1][b-1] = adjl[a-1].size()-1;
-            inds[b-1][a-1] = adjl[b-1].size()-1;
-        }
-        else{
-            if(c+d < adjl[a-1][inds[a-1][b-1]].second.first + adjl[a-1][inds[a-1][b-1]].second.second){
-                adjl[a-1][inds[a-1][b-1]].second = {c,d};
-                adjl[b-1][inds[b-1][a-1]].second = {c,d};
-            }
-        }
+        adjl[a-1].pb(b,make_pair(c,d));
+        adjl[b-1].pb(a,make_pair(c,d));
+        
+        // int a1 = min(a,b);
+        // int b1 = max(a,b);
+        
+        // if((wts[a1-1][b1-1].first==INT_MAX) || (c+d < wts[a1-1][b1-1].first + wts[a1-1][b1-1].second)){
+        //     wts[a1-1][b1-1] = {c,d};
+        // }
     }
 
-    cout << djikstra(adjl) << endl;
+    ll out = djikstra(adjl,n);
+    if(out==LLONG_MAX){
+        cout << -1 << endl;
+    }
+    else{
+        cout << out << endl;
+    }
 }
